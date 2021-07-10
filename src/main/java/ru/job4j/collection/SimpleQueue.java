082fbc2565e5ea89bb884938abb8d3f4169e8914
leonidkurmyshkin <leonid2073@gmail.com>
2021-07-10 12:53:11 +0300

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private int inSize = 0;
+    private final SimpleStack<T> out = new SimpleStack<>();
+    private int outSize = 0;
+
+    public T poll() {
+        if (outSize == 0) {
+            for (; inSize > 0; inSize--) {
+                out.push(in.pop());
+                outSize++;
+            }
+        }
+        outSize--;
+        return out.pop();
+    }
+
+    public void push(T value) {
+        in.push(value);
+        inSize++;
+    }
+}
