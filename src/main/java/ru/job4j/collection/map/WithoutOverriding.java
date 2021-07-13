f731ea9f1c9f80543423160270036624e14451d1
leonidkurmyshkin <leonid2073@gmail.com>
2021-07-13 15:05:50 +0300

    2. Без переопределения equals и hashCode [#1005]

---
+++ src/main/java/ru/job4j/collection/map/WithoutOverriding.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection.map;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WithoutOverriding {
+    public static void main(String[] args) {
+        var user1 = new User("Ковалев Павел", 2,
+                new GregorianCalendar(1960, Calendar.JANUARY, 1));
+        var user2 = new User("Ковалев Павел", 2,
+                new GregorianCalendar(1960, Calendar.JANUARY, 1));
+        Map<User, Object> users = new HashMap<>();
+        users.put(user1, new Object());
+        users.put(user2, new Object());
+        users.forEach((user, object) ->
+                System.out.println("User: " + user + " Object " + object));
+    }
+}
