8c6438dffd3c77ca382c2397cd359d0e20793531
leonidkurmyshkin <leonid2073@gmail.com>
2021-07-23 12:30:47 +0300

    0.2. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -4,21 +4,21 @@ import java.io.FileInputStream;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
+        var textNumbers = new StringBuilder();
         try (var in = new FileInputStream("even.txt")) {
-            var textNumbers = new StringBuilder();
             int readByte;
             while ((readByte = in.read()) != -1) {
                 textNumbers.append((char) readByte);
             }
-            for (var textNumber : textNumbers.toString()
-                    .split(System.lineSeparator())) {
-                System.out.print(textNumber);
-                System.out.println(Integer.parseInt(textNumber) % 2 == 0
-                            ? " - чётное число."
-                            : " - нечётное число.");
-            }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        for (var textNumber : textNumbers.toString()
+                .split(System.lineSeparator())) {
+            System.out.print(textNumber);
+            System.out.println(Integer.parseInt(textNumber) % 2 == 0
+                        ? " - чётное число."
+                        : " - нечётное число.");
         }
     }
 }
