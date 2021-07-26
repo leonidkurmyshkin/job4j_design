fd662d0ea2d8d9e22e10b4066148802ee6fbf252
leonidkurmyshkin <leonid2073@gmail.com>
2021-07-26 12:59:52 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (var read = new BufferedReader(new FileReader(path))) {
+            read.lines()
+                    .filter(line -> !line.isEmpty() && !line.startsWith("#"))
+                    .filter(line -> line.matches("^[a-z][\\w.]*=[^=]+$"))
+                    .map(line -> line.split("="))
+                    .forEach(words -> values.put(words[0], words[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("Неверный ключ, или значение.");
+        }
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"),is("Petr Arsentev"));
+    }
+
+    @Test
+    public void whenPairsWithCommentsAndEmptyLines() {
+        String path = "./data/whenPairsWithCommentsAndEmptyLines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Ivan Petrov"));
+        assertThat(config.value("team.currentPosition"), is("junior developer"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongKey() {
+        String path = "./data/whenWrongKey.properties";
+        Config config = new Config(path);
+        config.load();
+        config.value("7name");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenEmptyValue() {
+        String path = "./data/whenEmptyValue.properties";
+        Config config = new Config(path);
+        config.load();
+        config.value("name");
+    }
+}
