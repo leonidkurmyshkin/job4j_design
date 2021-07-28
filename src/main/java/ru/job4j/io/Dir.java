c5173d7bdbe194a4d43cfb26dd11eb4050c9f007
leonidkurmyshkin <leonid2073@gmail.com>
2021-07-28 15:12:29 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:/projects/job4j_design");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(
+                    String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(
+                    String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File subFile : file.listFiles()) {
+            if (subFile.isFile()) {
+                System.out.printf("%s size : %d bytes%n",
+                        subFile.getName(), subFile.length());
+            }
+        }
+    }
+}
