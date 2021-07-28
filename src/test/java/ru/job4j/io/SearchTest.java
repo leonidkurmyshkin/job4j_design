e6fa277a04de1b67692369d0989f204f9a85e670
leonidkurmyshkin <leonid2073@gmail.com>
2021-07-28 17:17:50 +0300

    4.1. Сканирование файловой системы. [#106929]

---
+++ src/test/java/ru/job4j/io/SearchTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class SearchTest {
+    @Test
+    public void whenLogsFound() throws IOException {
+        Path start = Paths.get(".");
+        List<Path> expected = List.of(
+                Paths.get(".\\data\\whenEndingIsUnknown.log"),
+                Paths.get(".\\data\\whenOneRangeUnavailable.log"),
+                Paths.get(".\\data\\whenPermanentAvailable.log"),
+                Paths.get(".\\data\\whenTwoRangeUnavailable.log")
+        );
+        var pathsRsl = Search.search(start,
+                p -> p.toString().endsWith(".log"));
+        assertThat(pathsRsl, is(expected));
+    }
+}
