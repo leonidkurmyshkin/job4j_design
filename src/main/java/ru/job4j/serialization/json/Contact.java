30918af2e604a2fabbc628041a82638520b43076
leonidkurmyshkin <leonid2073@gmail.com>
2021-09-16 14:14:18 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

---
+++ src/main/java/ru/job4j/serialization/json/Contact.java
@@ -0,0 +1,27 @@
+package ru.job4j.serialization.json;
+
+public class Contact {
+    private final int zipCode;
+    private final String phone;
+
+    public Contact(int zipCode, String phone) {
+        this.zipCode = zipCode;
+        this.phone = phone;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{"
+                + "zipCode=" + zipCode
+                + ", phone='" + phone + '\''
+                + '}';
+    }
+}
--- src/main/java/ru/job4j/serialization/json/Person.java
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -1,8 +1,7 @@
 package ru.job4j.serialization.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import ru.job4j.serialization.java.Contact;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -21,6 +20,26 @@ public class Person {
         this.statuses = statuses;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public String[] getStatuses() {
+        return statuses;
+    }
+
     @Override
     public String toString() {
         return "Person{"
@@ -33,13 +52,17 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        var person = new Person("Ivanov", false, 30,
-                new Contact(123456, "+7 (111) 111-11-99"), "Worker", "Married");
-        System.out.println(person);
-        final Gson gson = new GsonBuilder().create();
-        String savedPerson = gson.toJson(person);
-        System.out.println(savedPerson);
-        person = gson.fromJson(savedPerson, Person.class);
-        System.out.println(person);
+        JSONArray jsonStatuses = new JSONArray(new String[]
+                {"Worker", "Married"});
+        JSONObject jsonContact = new JSONObject()
+                .put("zipCode", 123456)
+                .put("phone", "+7 (111) 111-11-99");
+        JSONObject jsonPerson = new JSONObject()
+                .put("name", "Ivanov")
+                .put("sex", false)
+                .put("age", 30)
+                .put("contact", jsonContact)
+                .put("statuses", jsonStatuses);
+        System.out.println(jsonPerson.toString());
     }
 }
