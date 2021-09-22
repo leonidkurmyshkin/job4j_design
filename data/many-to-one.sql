e410c30abd716407f9c8751a22424ef3818757cf
leonidkurmyshkin <leonid2073@gmail.com>
2021-09-22 10:58:46 +0300

    0.1. Связи между таблицами [#373615]. Исправил

--- data/many-to-one.sql
+++ data/many-to-one.sql
@@ -1,9 +1,9 @@
-create table students(
+create table groups(
     id serial primary key,
     name varchar(255)
 );
-create table groups(
+create table students(
     id serial primary key,
-    students_id int references students(id),
-    name varchar(255)
+    name varchar(255),
+    groups_id int references groups(id)
 );
