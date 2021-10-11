4191b97775ba0d69ee5366cfbaf7fa20c6396939
leonidkurmyshkin <leonid2073@gmail.com>
2021-10-11 14:50:09 +0300

    2. Поиск файлов по критерию [#783]

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 [![Build Status](https://travis-ci.com/leonidkurmyshkin/job4j_design.svg?branch=master)](https://travis-ci.com/leonidkurmyshkin/job4j_design)
 [![codecov](https://codecov.io/gh/leonidkurmyshkin/job4j_design/branch/master/graph/badge.svg?token=LZRLYZH7W8)](https://codecov.io/gh/leonidkurmyshkin/job4j_design)
+# job4j_design
+Проект содержит решения из модулей "Структуры данных и алгоритмы", "Ввод-вывод", "SQL, JDBC"
+уровня "Джуниор" курса [Job4j](https://job4j.ru/).
---
+++ src/test/java/ru/job4j/io/find/FilesFinderTest.java
@@ -0,0 +1,133 @@
+package ru.job4j.io.find;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class FilesFinderTest {
+    public static File file1, file2, file3, file4;
+
+    @ClassRule
+    public static TemporaryFolder temp = new TemporaryFolder();
+
+    @BeforeClass
+    public static void createFiles() throws IOException {
+        file1 = temp.newFile("f1.txt");
+        file2 = temp.newFile("f2.txt");
+        file3 = temp.newFile("f3.log");
+        file4 = temp.newFile("f.csv");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWrongNumberOfArguments() {
+        var args = "-d=./data -n=.*\\.txt|.*\\.csv -o=./data/log.txt"
+                .split(" ");
+        FilesFinder.validateCommandLine(args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenFindFolderIsNotSet() {
+        var args = "-d= -n=.*\\.txt|.*\\.csv -t=regex -o=./data/log.txt"
+                .split(" ");
+        FilesFinder.validateCommandLine(args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenFindFolderNotExist() {
+        var args = "-d=./da -n=.*\\.txt|.*\\.csv -t=regex -o=./data/log.txt"
+                .split(" ");
+        FilesFinder.validateCommandLine(args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenFindFolderIsNotFolder() {
+        var args = "-d=./data/car.sql -n=.*\\.txt|.*\\.csv -t=regex -o=./data/log.txt"
+                .split(" ");
+        FilesFinder.validateCommandLine(args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenFindTypeIsNotSet() {
+        var args = "-d=./data -n=.*\\.txt|.*\\.csv -t= -o=./data/log.txt"
+                .split(" ");
+        FilesFinder.validateCommandLine(args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenFindTypeIsInvalid() {
+        var args = "-d=./data -n=.*\\.txt|.*\\.csv -t=yui7 -o=./data/log.txt"
+                .split(" ");
+        var argsName = FilesFinder.validateCommandLine(args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenFindNameIsNotSet() {
+        var args = "-d=./data -n= -t=regex -o=./data/log.txt"
+                .split(" ");
+        FilesFinder.validateCommandLine(args);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenFindNameIsInvalid() {
+        var args = "-d=./data -n=.** -t=regex -o=./data/log.txt"
+                .split(" ");
+        FilesFinder.validateCommandLine(args);
+    }
+
+    @Test
+    public void whenFindTypeIsMask() throws IOException {
+        String[] args = "-d=%s -n=*.txt -t=mask -o=%s/log.txt"
+                .formatted(temp.getRoot().getAbsolutePath(), temp.getRoot().getAbsolutePath())
+                .split(" ");
+        var argsName = FilesFinder.validateCommandLine(args);
+        var finder = new FilesFinder(argsName.get("d"), argsName.get("n"),
+                argsName.get("t"), argsName.get("o"));
+        finder.writeLog(finder.search());
+        try (var in = new BufferedReader(new FileReader(argsName.get("o"),
+                Charset.forName("Windows-1251")))) {
+            assertEquals(file1.getAbsolutePath(), in.readLine());
+            assertEquals(file2.getAbsolutePath(), in.readLine());
+            assertNull(in.readLine());
+        }
+    }
+
+    @Test
+    public void whenFindTypeIsName() throws IOException {
+        String[] args = "-d=%s -n=f.csv -t=name -o=%s/log.txt"
+                .formatted(temp.getRoot().getAbsolutePath(), temp.getRoot().getAbsolutePath())
+                .split(" ");
+        var argsName = FilesFinder.validateCommandLine(args);
+        var finder = new FilesFinder(argsName.get("d"), argsName.get("n"),
+                argsName.get("t"), argsName.get("o"));
+        finder.writeLog(finder.search());
+        try (var in = new BufferedReader(new FileReader(argsName.get("o"),
+                Charset.forName("Windows-1251")))) {
+            assertEquals(file4.getAbsolutePath(), in.readLine());
+            assertNull(in.readLine());
+        }
+    }
+
+    @Test
+    public void whenFindTypeIsRegex() throws IOException {
+        String[] args = "-d=%s -n=.*\\.log|.*\\.csv -t=regex -o=%s/log.txt"
+                .formatted(temp.getRoot().getAbsolutePath(), temp.getRoot().getAbsolutePath())
+                .split(" ");
+        var argsName = FilesFinder.validateCommandLine(args);
+        var finder = new FilesFinder(argsName.get("d"), argsName.get("n"),
+                argsName.get("t"), argsName.get("o"));
+        finder.writeLog(finder.search());
+        try (var in = new BufferedReader(new FileReader(argsName.get("o"),
+                Charset.forName("Windows-1251")))) {
+            assertEquals(file4.getAbsolutePath(), in.readLine());
+            assertEquals(file3.getAbsolutePath(), in.readLine());
+            assertNull(in.readLine());
+        }
+    }
+}
