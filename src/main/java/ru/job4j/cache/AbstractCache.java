50958eabb9acc072c4d2a40d182fc2d2fd5e413a
leonidkurmyshkin <leonid2073@gmail.com>
2021-11-03 14:03:47 +0300

    1. Реализация кеша на SoftReference [#1592]. Исправил AbstractCache

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -8,14 +8,19 @@ public abstract class AbstractCache<K, V> {
     protected final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public void put(K key, V value) {
-        cache.putIfAbsent(key, new SoftReference<>(value));
+        cache.put(key, new SoftReference<>(value));
     }
 
     public V get(K key) {
-        if (!cache.containsKey(key)) {
-            put(key, load(key));
+        V value = null;
+        if (cache.containsKey(key)) {
+            value = cache.get(key).get();
         }
-        return cache.get(key).get();
+        if (value == null) {
+            value = load(key);
+            put(key, value);
+        }
+        return value;
     }
 
     protected abstract V load(K key);
